ac35a5e3c8d2b48e61e5ab4a2166028b9545c558
Luke Henderson <personal@lukeh.us>
2025-07-30 10:43:20 +0900

    i forgor vec2 hahaball

--- src/math.c
+++ src/math.c
@@ -3,39 +3,57 @@
 // Helper macro for mat4 indexing (column-major: m[col*4 + row])
 #define MAT4_IDX(row, col) ((col) * 4 + (row))
 
-vec3 vec3_add(vec3 a, vec3 b) {
-    return (vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+// VEC2
+vec2 vec2_add (vec2 a, vec2 b) {
+    return (vec2){a.x + b.x, a.y + b.y};
 }
-
-vec3 vec3_sub(vec3 a, vec3 b) {
-    return (vec3){a.x - b.x, a.y - b.y, a.z - b.z};
+vec2 vec2_sub(vec2 a, vec2 b) {
+    return (vec2){a.x - b.x, a.y - b.y};
 }
-
-vec3 vec3_mul_scalar(vec3 v, float s) {
-    return (vec3){v.x * s, v.y * s, v.z * s};
+vec2 vec2_scale(vec2 v, float s) {
+    return (vec2){v.x * s, v.y * s};
 }
-
-vec3 vec3_normalize(vec3 v) {
-    float len = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+vec2 vec2_normalize(vec2 v) {
+    float len = sqrtf(v.x * v.x + v.y * v.y);
     if (len > 0.0f) {
-        return vec3_mul_scalar(v, 1.0f / len);
+        return vec2_scale(v, 1.0f / len);
     }
     return v;  // zero vector case
 }
+float vec2_dot(vec2 a, vec2 b) { return a.x * b.x + a.y * b.y; }
+float vec2_cross(vec2 a, vec2 b) {
+    return a.x * b.y - a.y * b.x;
+}
 
+// VEC3
+vec3 vec3_add(vec3 a, vec3 b) {
+    return (vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+vec3 vec3_sub(vec3 a, vec3 b) {
+    return (vec3){a.x - b.x, a.y - b.y, a.z - b.z};
+}
+vec3 vec3_scale(vec3 v, float s) {
+    return (vec3){v.x * s, v.y * s, v.z * s};
+}
+vec3 vec3_normalize(vec3 v) {
+    float len = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+    if (len > 0.0f) {
+        return vec3_scale(v, 1.0f / len);
+    }
+    return v;  // zero vector case
+}
 float vec3_dot(vec3 a, vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-
 vec3 vec3_cross(vec3 a, vec3 b) {
     return (vec3){a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
                   a.x * b.y - a.y * b.x};
 }
 
+// MAT4
 void mat4_identity(mat4 m) {
     for (int i = 0; i < 16; i++) m[i] = 0.0f;
     m[MAT4_IDX(0, 0)] = m[MAT4_IDX(1, 1)] = m[MAT4_IDX(2, 2)] =
         m[MAT4_IDX(3, 3)]                 = 1.0f;
 }
-
 void mat4_translate(mat4 m, vec3 v) {
     mat4 trans;
     mat4_identity(trans);
@@ -44,7 +62,6 @@ void mat4_translate(mat4 m, vec3 v) {
     trans[MAT4_IDX(2, 3)] = v.z;
     mat4_multiply(m, m, trans);  // post-multiply
 }
-
 void mat4_rotate_x(mat4 m, float angle_rad) {
     float c = cosf(angle_rad), s = sinf(angle_rad);
     mat4 rot;
@@ -55,7 +72,6 @@ void mat4_rotate_x(mat4 m, float angle_rad) {
     rot[MAT4_IDX(2, 2)] = c;
     mat4_multiply(m, m, rot);
 }
-
 void mat4_rotate_y(mat4 m, float angle_rad) {
     float c = cosf(angle_rad), s = sinf(angle_rad);
     mat4 rot;
@@ -66,7 +82,6 @@ void mat4_rotate_y(mat4 m, float angle_rad) {
     rot[MAT4_IDX(2, 2)] = c;
     mat4_multiply(m, m, rot);
 }
-
 void mat4_rotate_z(mat4 m, float angle_rad) {
     float c = cosf(angle_rad), s = sinf(angle_rad);
     mat4 rot;
@@ -77,7 +92,6 @@ void mat4_rotate_z(mat4 m, float angle_rad) {
     rot[MAT4_IDX(1, 1)] = c;
     mat4_multiply(m, m, rot);
 }
-
 void mat4_scale(mat4 m, vec3 v) {
     mat4 scale;
     mat4_identity(scale);
@@ -86,7 +100,6 @@ void mat4_scale(mat4 m, vec3 v) {
     scale[MAT4_IDX(2, 2)] = v.z;
     mat4_multiply(m, m, scale);
 }
-
 void mat4_multiply(mat4 out, mat4 a, mat4 b) {
     mat4 temp;
     for (int row = 0; row < 4; row++) {
@@ -100,7 +113,6 @@ void mat4_multiply(mat4 out, mat4 a, mat4 b) {
     }
     for (int i = 0; i < 16; i++) out[i] = temp[i];
 }
-
 void mat4_perspective(
     mat4 m, float fov_rad, float aspect, float near, float far
 ) {
@@ -113,7 +125,6 @@ void mat4_perspective(
     m[MAT4_IDX(3, 2)] = -(2.0f * far * near) / (far - near);
     m[MAT4_IDX(3, 3)] = 0.0f;
 }
-
 void mat4_look_at(mat4 m, vec3 eye, vec3 center, vec3 up) {
     vec3 f = vec3_normalize(vec3_sub(center, eye));
     vec3 s = vec3_normalize(vec3_cross(f, up));
