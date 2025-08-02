b06562629fb1aec7be520a3b75964140bc7f56a6
Luke Henderson <personal@lukeh.us>
2025-08-02 21:28:42 +0900

    added vec4, quat stuff

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// Helper macro for mat4 indexing (column-major: m[col*4 + row])
+#define MAT4_IDX(row, col) ((col) * 4 + (row))
+
 typedef struct {
     float x, y;
 } vec2;
@@ -34,11 +37,18 @@ vec4 vec4_scale (vec4 v, float s);
 vec4 vec4_normalize (vec4 v);
 float vec4_dot (vec4 a, vec4 b);
 
+vec4 quat_from_euler(vec3 euler);
+vec3 euler_from_quat(vec4 q);
+vec4 quat_multiply(vec4 a, vec4 b); // compose rotations
+vec4 quat_conjugate(vec4 q);
+vec4 quat_normalize (vec4 q);
+
 void mat4_identity(mat4 m);
 void mat4_translate(mat4 m, vec3 v);
 void mat4_rotate_x(mat4 m, float angle_rad);
 void mat4_rotate_y(mat4 m, float angle_rad);
 void mat4_rotate_z(mat4 m, float angle_rad);
+void mat4_rotate_quat(mat4 m, vec4 q);
 void mat4_scale(mat4 m, vec3 v);
 void mat4_multiply(mat4 out, mat4 a, mat4 b);
 void mat4_perspective(mat4 m, float fov_rad, float aspect, float near, float far);
