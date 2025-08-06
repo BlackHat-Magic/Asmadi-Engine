fd4291dfafe47c834e73ef1dbb75167684f21f71
Luke Henderson <personal@lukeh.us>
2025-08-06 23:24:03 +0900

    rearchitected; this def should not have been on master lmao

--- examples/demo/main.c
+++ examples/demo/main.c
@@ -5,8 +5,6 @@
 #include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_timer.h>
-#include "geometry/cylinder.h"
-#include "geometry/tetrahedron.h"
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include <SDL3/SDL.h>
@@ -16,24 +14,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "core/appstate.h"
-#include "ecs/ecs.h"
-#include "geometry/box.h"
-#include "geometry/capsule.h"
-#include "geometry/circle.h"
-#include "geometry/cone.h"
-#include "geometry/dodecahedron.h"
-#include "geometry/icosahedron.h"
-#include "geometry/octahedron.h"
-#include "geometry/plane.h"
-#include "geometry/ring.h"
-#include "geometry/sphere.h"
-#include "geometry/torus.h"
-#include "geometry/tetrahedron.h"
-#include "material/m_common.h"
-#include "material/basic_material.h"
-#include "material/phong_material.h"
-#include "math/matrix.h"
+#include <core/appstate.h>
+#include <ecs/ecs.h>
+#include <geometry/box.h>
+#include <geometry/capsule.h>
+#include <geometry/circle.h>
+#include <geometry/cone.h>
+#include "geometry/cylinder.h"
+#include <geometry/dodecahedron.h>
+#include <geometry/icosahedron.h>
+#include <geometry/octahedron.h>
+#include <geometry/plane.h>
+#include <geometry/ring.h>
+#include <geometry/sphere.h>
+#include <geometry/torus.h>
+#include <geometry/tetrahedron.h>
+#include <material/m_common.h>
+#include <material/basic_material.h>
+#include <material/phong_material.h>
+#include <math/matrix.h>
 
 #define STARTING_WIDTH 640
 #define STARTING_HEIGHT 480
@@ -311,7 +310,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     // transform
     add_transform(dodecahedron, (vec3){4.0f, 4.0f, 0.0f}, (vec3){0.0f, 0.0f, 0.0f}, (vec3){1.0f, 1.0f, 1.0f});
 
-    // octahedron
+    // icosahedron
     Entity icosahedron = create_entity();
     // mesh
     MeshComponent icosahedron_mesh = create_icosahedron_mesh(0.5f, state->device);
